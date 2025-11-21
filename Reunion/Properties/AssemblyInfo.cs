52c7c9620dda371faeb4895efee114f2cf8e3c70
Shiroko3 <mhy1630005@163.com>
2025-11-21 16:44:18 +0800

    正式发布1.5.1

--- Reunion/Properties/AssemblyInfo.cs
+++ Reunion/Properties/AssemblyInfo.cs
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 //
 [assembly: AssemblyVersion("1.5.1.0")]
 [assembly: AssemblyFileVersion("1.5.1.0")]
-[assembly: AssemblyInformationalVersion("1.5.1-beta")]
+[assembly: AssemblyInformationalVersion("1.5.1")]
